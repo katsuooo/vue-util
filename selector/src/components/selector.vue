aa29e776eba3757699a7b9e4e6306f0d4884eb61
taka katsu <katsuooo.g@gmail.com>
2020-08-28 19:43:47 +0900

    add vue-select sample

---
+++ selector/src/components/selector.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <vSelect :options="['aaa','bbb','ccc']" v-model="selected" @input='setSelected(selected)' />
+    </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+export default {
+    components:{
+        vSelect
+    },
+    data: () => ({
+        selected: ''
+    }),
+    methods:{
+        setSelected: (selectedx) => {
+            console.log(selectedx)
+            console.log('test')
+        }
+    }
+}
+</script>
+
+<style>
+@import '@/../../../node_modules/vue-select/dist/vue-select.css';
+
+</style>
